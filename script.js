769ecab4b7e5bb10fde21e3ccd5bdab937c13dd1
Aarti-bisen589 <aartibisen2004@gmail.com>
2023-09-11 16:45:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+// var timer = document.querySelector("#timer h1")
+// var timerButton = document.querySelector("#timer button")
+
+// var audio = new Audio("./insect-sound.mp3")
+// var grow  = 0
+// var int = setInterval(function(){
+//     if(grow < 90){ 
+//         grow += Math.floor(Math.random()*20)
+//         timer.innerHTML = grow+"%"
+//     } else{
+//         grow = 100
+//         timer.innerHTML = grow+"%"
+        
+//         timer.style.transform = "translateY(-100%)"
+//         timerButton.style.transform = "translateY(-100%)"
+//         timerButton.style.opacity = "1"
+
+
+//         clearInterval(int)
+       
+//     }
+// },Math.floor(Math.random()*300))
+
+
+// timerButton.addEventListener("click",function(){
+//     audio.play()
+//     gsap.to("#page1",{
+//         scale:1,
+//         duration:0.5
+//     })
+//     gsap.to("#timer",{
+//         opacity:0
+//     })
+//     gsap.to("#log",{
+//         opacity: 1
+//     })
+// })
+
+var timer = document.querySelector("#timer h1")
+var timerButton = document.querySelector("#timer button")
+
+var audio = new Audio("./insect-sound.mp3")
+
+var grow  = 0
+var int = setInterval(function(){
+    if(grow < 90){ 
+        grow += Math.floor(Math.random()*20)
+        timer.innerHTML = grow+"%"
+    } else{
+        grow = 100
+        timer.innerHTML = grow+"%"
+      
+        timer.style.transform = "translateY(-100%)"
+        timerButton.style.transform = "translateY(-100%)"
+        timerButton.style.opacity = "1"
+
+
+        clearInterval(int)
+       
+    }
+},Math.floor(Math.random()*300))
+
+
+timerButton.addEventListener("click",function(){
+    audio.play()
+    gsap.to("#page1",{
+        scale:1,
+        duration:0.5
+    })
+    gsap.to("#timer",{
+        opacity:0
+    })
+    gsap.to("#log",{
+        opacity:1,
+        
+    })
+
+    gsap.to("#patti",{
+        opacity:1,
+        
+    })
+gsap.to("#nav",{
+    opacity: 1,
+    top: 0
+
+})
+gsap.to("#main",{
+    overflow: "auto",
+})
+
+gsap.to("#circle",{
+    opacity:1,
+    delay: 2
+    
+})
+
+gsap.to("#lorem",{
+    opacity:1,
+    x: 130,
+    duration:5,
+    
+    
+    
+    
+    
+})
+
+})
